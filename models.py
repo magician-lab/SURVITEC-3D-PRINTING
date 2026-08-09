a8e4c2d3945b18451bc455c0801663f6cf8bb861
magician-lab <magicdevelopers9@gmail.com>
2026-08-09 19:21:34 +0300

    Update models.py

--- models.py
+++ models.py
@@ -817,45 +817,6 @@ class Result(db.Model):
     )
 
 
-# ==========================================================
-# SUBSCRIPTION
-# ==========================================================
-
-class Subscription(db.Model):
-
-    __tablename__ = "subscriptions"
-
-    id = db.Column(
-        db.String(36),
-        primary_key=True,
-        default=generate_uuid
-    )
-
-    school_id = db.Column(
-        db.String(36),
-        db.ForeignKey(
-            "schools.id",
-            ondelete="CASCADE"
-        ),
-        nullable=True
-    )
-
-    amount = db.Column(
-        db.Float
-    )
-
-    start_date = db.Column(
-        db.Date
-    )
-
-    expiry_date = db.Column(
-        db.Date
-    )
-
-    status = db.Column(
-        db.String(20)
-    )
-
 
 # ==========================================================
 # CONSENT
