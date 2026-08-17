92714834e16ef19647b245123cf415fb8e605467
magician-lab <magicdevelopers9@gmail.com>
2026-08-17 13:15:30 +0300

    PORT CHANGE

--- main.py
+++ main.py
@@ -133,11 +133,13 @@ with app.app_context():
 
 
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 465
-app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True   # Switch this to True
+app.config['MAIL_USE_SSL'] = False  # Switch this to False
 app.config['MAIL_USERNAME'] = 'survitec3d@gmail.com'
 app.config['MAIL_PASSWORD'] = 'ccbg lauz xxha obhp'
 app.config['MAIL_DEFAULT_SENDER'] = 'SURVITEC 3D <survitec3d@gmail.com>'
+
 
 mail = Mail(app)
 
