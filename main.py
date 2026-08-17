defe7e363e297e30a68ed933f5bb1ab15302c376
magician-lab <magicdevelopers9@gmail.com>
2026-08-17 12:46:51 +0300

    SMTP

--- main.py
+++ main.py
@@ -136,7 +136,7 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USE_SSL'] = True
 app.config['MAIL_USERNAME'] = 'survitec3d@gmail.com'
-app.config['MAIL_PASSWORD'] = 'sqzw dhef ysgg mjdt'
+app.config['MAIL_PASSWORD'] = 'ccbg lauz xxha obhp'
 app.config['MAIL_DEFAULT_SENDER'] = 'SURVITEC 3D <survitec3d@gmail.com>'
 
 mail = Mail(app)
